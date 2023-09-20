1285f21eb27557dbeb10a38a511d40cb0aced8ac
Natasha Miquele <114088618+natymiquele08@users.noreply.github.com>
2023-09-20 17:32:54 -0300

    seguir probando boton de borrar

--- script.js
+++ script.js
@@ -1,31 +1,40 @@
+const DATA_URL= 'https://jsonplaceholder.typicode.com/users'
+const container = document.getElementById("data");
 
-fetch('https://jsonplaceholder.typicode.com/users')
-.then(response => response.json())
-.then(data => mostrarData(data))
-
-const mostrarData = (data) => {
-    console.log(data)
-    let body= ''
-    for(let i=0; i<data.length; i++){
-        body += 
+function showData(dataArray){
+    for (const item of dataArray){
+        container.innerHTML += 
         ` 
         <tr> 
-        <td>${data[i].name}</td>
-        <td>${data[i].username}</td>
-        <td>${data[i].email}</td>
-        
-        <td>${data[i].phone}</td>
-        <td>${data[i].website}</td>
+        <td>${item.name}</td>
+        <td>${item.username}</td>
+        <td>${item.email}</td>
+        <td>${item.phone}</td>
+        <td>${item.website}</td>
 
         <td> <button id="eliminarBtn"> Eliminar </button></td>
         
         </tr>
         `
     }
-
-    document.getElementById("data").innerHTML= body
-    
 }
+
+
+fetch(DATA_URL)
+.then(response => response.json())
+.then(data => {
+    showData(data)
+})
+
+setTimeout(showData(data), 1500);
+
+
+
+    
+
+
+
+
 
 
 
